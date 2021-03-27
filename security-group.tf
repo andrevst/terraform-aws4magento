8a26cd924a319e944747e5626fb4164d417fb8bc
andrevst <andreveigatonelli@gmail.com>
2021-03-27 10:50:10 -0300

    feat: terraform ec2 with security group

---
+++ security-group.tf
@@ -0,0 +1,28 @@
+#security-group.tf
+ 
+resource "aws_security_group" "Security_Group" {
+  name = "Security-Group"
+  description = "Allowing ssh and http traffic"
+ 
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
--- variables.tf
+++ variables.tf
@@ -9,3 +9,27 @@ variable "access_key" {
 variable "secret_key" {
   type = string
 }
+
+variable "type" {
+  default = "t2.micro"			#Enter Instance Type (ex - t2.micro)
+}
+ 
+variable "key_name" {
+  default = "terraforming-magento"			#my pem key
+}
+
+variable "key_test" {
+  default = "scandiweb-key"			#test pem key
+}
+ 
+variable "region" {
+  default = "us-east-2"			#Enter aws region code (ex - ap-south-1)
+}
+ 
+variable "pem_file" {
+  default = "terraforming-magento.pem"			#Enter Path of pem file
+}
+ 
+variable "ec2_tag" {
+  default = "test-magento-server"			#Give name to EC2 instance
+}
